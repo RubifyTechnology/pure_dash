18eb8d3d4aad6d9abe40bacdc6a3898f4b1e8176
James <james@rubify.com>
2014-07-14 14:17:47 +0800

    add pickadate and slider on off for welcome setting

--- app/assets/javascripts/pure_dash/app.js
+++ app/assets/javascripts/pure_dash/app.js
@@ -1,3 +1,6 @@
 //= require smart_listing
 //= require ./float_label
 //= require ./responsive-tables
+//= require ../pickadate/picker.js
+//= require ../pickadate/picker.date.js
+//= require ../pickadate/legacy.js
