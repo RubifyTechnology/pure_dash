87041d42edc102f9e331c2e7eedd5f77607b375b
Karl <karl@rubify.com>
2014-07-15 15:20:05 +0700

    Add: Password strength

--- app/assets/javascripts/pure_dash/app.js
+++ app/assets/javascripts/pure_dash/app.js
@@ -1,6 +1,7 @@
 //= require smart_listing
 //= require ./float_label
 //= require ./responsive-tables
+//= require ./strength
 //= require ../pickadate/picker.js
 //= require ../pickadate/picker.date.js
 //= require ../pickadate/legacy.js
