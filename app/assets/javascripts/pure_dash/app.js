07c3f23eda24e46e60315d35c88888faa08b9ccb
Karl <karl@rubify.com>
2014-07-15 14:19:32 +0700

    Style for messages

--- app/assets/javascripts/pure_dash/app.js
+++ app/assets/javascripts/pure_dash/app.js
@@ -1,7 +1,6 @@
 //= require smart_listing
 //= require ./float_label
 //= require ./responsive-tables
-//= require ./form_action
 //= require ../pickadate/picker.js
 //= require ../pickadate/picker.date.js
 //= require ../pickadate/legacy.js
