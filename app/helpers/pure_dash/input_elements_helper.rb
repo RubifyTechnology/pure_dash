b375f51138c9dec16e264fedb5aeb6d9652eaecd
James Huynh <james@rubify.com>
2014-08-04 20:38:00 +0800

    update on off switch

--- app/helpers/pure_dash/input_elements_helper.rb
+++ app/helpers/pure_dash/input_elements_helper.rb
@@ -1,13 +1,13 @@
 module PureDash
   module InputElementsHelper
     def pure_dash_on_off_switch(opts={})
-      content_tag(:div, [content_tag(:input, '', type: "hidden", name: opts[:name] || "onoffswitch", value:'0'), content_tag(:input, '', type: "checkbox", name: opts[:name] || "onoffswitch", class: "onoffswitch-checkbox", id: "myonoffswitch", checked: (opts[:state].nil? ? true : opts[:state]), value:'1'),
-        content_tag(:label, 
+      content_tag(:div, [content_tag(:input, '', type: "hidden", name: opts[:name] || "onoffswitch", value:'0'), content_tag(:input, '', type: "checkbox", name: opts[:name] || "onoffswitch", class: "onoffswitch-checkbox", id: (opts[:id] || "myonoffswitch"), checked: (opts[:state].nil? ? true : opts[:state]), value:'1'),
+        content_tag(:label,
           [
             content_tag(:span, '', class: "onoffswitch-inner"),
             content_tag(:span, '', class: "onoffswitch-switch"),
           ].join.html_safe,
-        class: "onoffswitch-label", for: "myonoffswitch")
+        class: "onoffswitch-label", for: (opts[:id] || "myonoffswitch"))
       ].join.html_safe , class: "onoffswitch")
     end
   end
