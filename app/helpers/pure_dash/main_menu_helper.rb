8c604baaefe60302681ea6381e0bc3dbbb025205
Karl <karl@rubify.com>
2014-08-05 13:59:45 +0700

    update menu helper

--- app/helpers/pure_dash/main_menu_helper.rb
+++ app/helpers/pure_dash/main_menu_helper.rb
@@ -4,18 +4,30 @@ module PureDash
       menus_content = []
       sub_menu_contents = []
       menus.each do |menu|
-        if menu[:active]          
-          if menu[:sub_menus]          
-            menu[:sub_menus].each do |sub_menu|
-              sub_menu_contents << content_tag(:li, content_tag(:a, sub_menu[:label], href: sub_menu[:url]), class: sub_menu[:active] ? 'active' : '')
-            end  
+        active = check_active_menus(menu[:active])
+        if active == "active" and menu[:sub_menus]
+          menu[:sub_menus].each do |sub_menu|
+            sub_menu_contents << content_tag(:li, content_tag(:a, sub_menu[:label], href: sub_menu[:url]), class: check_active_menus(sub_menu[:active]))
           end
         end
-        menus_content << content_tag(:li, menu[:content] ? menu[:content] : content_tag(:a, menu[:label], href: menu[:url]), class: menu[:active] ? 'active' : '')
-        
+        menus_content << content_tag(:li, menu[:content] ? menu[:content] : content_tag(:a, menu[:label], href: menu[:url]), class: active)
       end
       
       content_tag(:div, content_tag(:div, content_tag(:ul, menus_content.join.html_safe, class: "pure-dash-main-menu"), class: "pure-dash-main-menu-container pure-menu pure-menu-open pure-menu-horizontal") + (sub_menu_contents.length > 0 ? content_tag(:div, content_tag(:ul, sub_menu_contents.join.html_safe), class: "pure-menu pure-menu-open pure-menu-horizontal pure-dash-sub-menu-container") : ""), class: "pure-dash-menu-container") + content_tag(:div, content_tag(:span, ""), class: "open-menu-link glyphicon glyphicon-align-justify")
     end
+    
+    protected
+    
+    def check_active_menus(active)
+      return active.select{|item| check_active_item(item)}.count > 0 ? 'active' : '' if active.is_a? Array
+      return check_active_item(active) ? 'active' : '' if active.is_a? String
+      return active ? 'active' : '' # True or False
+    end
+    
+    def check_active_item(item)
+      return item if !!item == item
+      (item.index('#').nil? and item == params[:controller]) or ("#{params[:controller]}##{params[:action]}" == item)
+    end
+      
   end
 end
